914212fe3cfa00caae8dce1fbaeae5ff82e04e0e
sato <sato.t.5970@gmail.com>
2025-04-28 13:55:07 +0900

    課題3

---
+++ 1Q/3/3-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 11;
+    int b = 23;
+    int sum = a + b;
+    int product = a * b;
+    printf("a= %d\n", a);
+    printf("b= %d\n", b);
+    printf("a+b= %d\n", sum);
+    printf("a*b= %d\n", product);
+    return 0;
+}
