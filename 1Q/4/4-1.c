fe6b1b21cccd1c2979588161766e9dc2665e6026
SatoTakumi <sato.t.5970@gmail.com>
2025-05-12 14:39:31 +0900

    課題4

---
+++ 1Q/4/4-1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float a = 1.0f;
+    float b = 3.0f;
+    float c = a / b;
+
+    printf("c = %.20f\n", c);
+
+    return 0;
+}
---
+++ 1Q/4/4-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    double a = 1.0;
+    double b = 3.0;
+    double c = a / b;
+
+    printf("c = %.20f\n", c);
+
+    return 0;
+}
